9ae42e1331bcd567caa15e4768dc4fa333577091
RozalinaRavi <violet_star@ukr.net>
2024-04-15 19:33:59 +0300

    calculation form done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,10 +1,16 @@
   const hamburger = document.querySelector('.hamburger')
   const headerNav = document.querySelector('.header-nav')
+  const fileInput = document.querySelector('#calculation-file');
+  const fileName = document.querySelector('#file-name')
 
   hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('is-active')
     headerNav.classList.toggle('show')
   })
+
+  fileInput.addEventListener('change', () => {
+    fileName.innerHTML = fileInput.files[0].name;
+});
 
   $('#first-screen-slider').slick({
      
@@ -19,3 +25,25 @@
   }, ]
   });
   
+
+  const controlledField = document.querySelectorAll('[type="checkbox"]')
+  const fileField = document.querySelectorAll('[type=file')
+  const formBtns = document.querySelectorAll('form .col button')
+ 
+
+
+  $('select').niceSelect();
+
+
+
+
+  const addParentClass = (array, parent, className) => {
+    array.forEach(item => {
+    item.closest(parent).classList.add(className)
+    })
+  }
+
+  addParentClass(controlledField, '.input-wrapper', 'controlled-wrapper')
+  addParentClass(controlledField, '.col', 'controlled-col')
+  addParentClass(fileField, '.input-wrapper', 'file-wrapper')
+  addParentClass(formBtns, '.col', 'mobile-center')
